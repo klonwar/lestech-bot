c0cf0918495c9411875a2cd8a93409ebb5ebe716
Klonwar <dima.arapov@gmail.com>
2023-08-04 22:00:27 +0300

    Replacing decorator with dynamic cron initialization in order to use config from .env

---
+++ src/cron/cron.constants.ts
@@ -0,0 +1,3 @@
+export enum CronJobNames {
+  UPDATE = 'update',
+}
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,31 +1,44 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { ListService } from '../list/list.service';
 import { BotService } from '../bot/bot.service';
+import { ConfigService } from '@nestjs/config';
+import { CronJob } from 'cron';
+import { CronJobNames } from './cron.constants';
 
 @Injectable()
-export class CronService {
-  // @TODO: Move into env variable
-  private static readonly PERIOD = 59;
+export class CronService implements OnModuleInit {
   private readonly logger = new Logger(CronService.name);
+  private jobs: Record<CronJobNames, CronJob> = {
+    [CronJobNames.UPDATE]: new CronJob(
+      this.configService.get<string>('CRON'),
+      () => this.update(),
+    ),
+  };
 
   constructor(
+    private configService: ConfigService,
+    private schedulerRegistry: SchedulerRegistry,
     private listService: ListService,
     private botService: BotService,
   ) {
     this.update();
   }
 
-  @Cron(`*/${CronService.PERIOD} * * * *`)
+  onModuleInit() {
+    Object.entries(this.jobs).forEach(([name, job]) => {
+      this.schedulerRegistry.addCronJob(name, job);
+      job.start();
+    });
+  }
+
   public async update() {
     const { list, updated } = await this.listService.update();
     if (updated) {
       await this.botService.notify(list);
     }
-    this.info();
-  }
-
-  private info() {
-    this.logger.verbose(`Next request in ${CronService.PERIOD} minutes`);
+    this.logger.verbose(
+      `Updated. Cron configuration: ${this.configService.get<string>('CRON')}`,
+    );
   }
 }
